12fd917c16cd99f40378108960f37f1738292054
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2023-03-24 11:01:30 -0500

    Add mobile icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -81,6 +81,26 @@ export default function HomePage({
     <div>
       <Head>
         <title>Dollar</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Header title={'Online dollar prices in Peru'} />
       <h2>Best places to sell</h2>
